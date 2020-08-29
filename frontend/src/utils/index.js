f1faa7242d9280ce2ddaf02aba0f824df2bddb5d
Yogendra Pandey <yogendrapandey@Yogendras-MacBook-Pro.local>
2020-08-29 23:59:25 +0530

    FILTER FUNCTION OPTIMIZATION

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,8 +1,7 @@
 
 export const filterNews = (searchText, news) => {
-    let filteredNews = [];
-    filteredNews = news.filter((item) => {
+    return news.filter((item) => {
         return (item.title && item.title.toLowerCase().indexOf(searchText.toLowerCase()) > -1) || (item.description && item.description.toLowerCase().indexOf(searchText.toLowerCase()) > -1);
     });
-    return filteredNews;
 }
+
